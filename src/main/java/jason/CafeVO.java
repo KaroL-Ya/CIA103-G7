b53b5347cf1ee66f6d6ca189612b92085ee559dc
06_Jason <jason841214850713@gmail.com>
2024-10-23 16:07:33 +0800

    test

--- src/main/java/jason/CafeVO.java
+++ src/main/java/jason/CafeVO.java
@@ -2,17 +2,10 @@ package jason;
 
 public class CafeVO {
 	private Integer cafeId;
-	private String cafeName;
 	public Integer getCafeId() {
 		return cafeId;
 	}
 	public void setCafeId(Integer cafeId) {
 		this.cafeId = cafeId;
-	}
-	public String getCafeName() {
-		return cafeName;
-	}
-	public void setCafeName(String cafeName) {
-		this.cafeName = cafeName;
 	}
 }
