e736e56408c2d904ad221508fce8c28d0a33d590
Hua <pollyhsu2001@gmail.com>
2024-10-23 16:39:23 +0800

    test2

--- src/main/java/hua/ItemVO.java
+++ src/main/java/hua/ItemVO.java
@@ -1,9 +1,8 @@
 package hua;
 
 public class ItemVO {
-	private Integer ItemId;
 	
 	public static void main(String[] args) {
-		System.out.println("item");
+		System.out.println("test1023");
 	}
 }
