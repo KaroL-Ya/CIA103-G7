90845331d93db10767d5de32f5dda2d3a1e63b7b
06_Jason <jason841214850713@gmail.com>
2024-12-16 13:54:18 +0800

    處理咖啡廳登出路徑問題

--- src/main/java/com/cafe/controller/CafeFrontController.java
+++ src/main/java/com/cafe/controller/CafeFrontController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.cafe.model.CafeService;
 import com.cafe.model.CafeVO;
 
+import java.util.Base64;
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -20,8 +23,6 @@ public class CafeFrontController {
     @Autowired
     HttpSession session;
 
-    
-       
     // 首頁
     @GetMapping("/cafe")
     public String frontendIndex(Model model) {
@@ -33,11 +34,10 @@ public class CafeFrontController {
     public String cafeLogin(Model model) {
         return "front-end/cafeLogin"; // view
     }
-    
+
     // 登入按鈕
     @GetMapping("/cafe/cafe_Login")
     public String cafe_Login(Model model) {
-    	
         return "front-end/index1"; // view
     }
 
@@ -53,19 +53,46 @@ public class CafeFrontController {
     @GetMapping("/cafe/cafeProfile")
     public String profile(Model model) {
         Integer cafeId = (Integer) session.getAttribute("cafeId"); // 獲取登錄的咖啡廳ID
-        model.addAttribute("cafeVO", cafeSvc.getOneCafe(cafeId));
+        CafeVO cafeVO = cafeSvc.getOneCafe(cafeId);
+
+        // 將圖片轉換為 Base64
+        if (cafeVO.getImg() != null) {
+            String base64Img = Base64.getEncoder().encodeToString(cafeVO.getImg());
+            cafeVO.setBase64Img(base64Img);
+        }
+
+        model.addAttribute("cafeVO", cafeVO);
         return "front-end/cafe/cafeprofile";
     }
-    
-    //後台首頁
+
+    // 後台首頁
     @GetMapping("/cafe/index2")
     public String backendIndex(Model model) {
         return "/index2";
     }
-    
-    //後台新增商家
+
+    // 後台新增商家
     @GetMapping("/cafe/select_page")
     public String backendSelect(Model model) {
+        List<CafeVO> list = cafeSvc.getAll();
+
+        // 將每個商家的圖片轉換為 Base64
+        convertImgToBase64(list);
+
+        System.out.println("CafeListData: " + list.size());
+        list.forEach(cafe -> System.out.println("商家ID: " + cafe.getCafeId() + ", 商家名稱: " + cafe.getName()));
+
+        model.addAttribute("cafeListData", list);
         return "back-end/cafe/select_page";
+    }
+
+    // 將圖片轉換為 Base64 的方法
+    private void convertImgToBase64(List<CafeVO> cafes) {
+        for (CafeVO cafe : cafes) {
+            if (cafe.getImg() != null) {
+                String base64Img = Base64.getEncoder().encodeToString(cafe.getImg());
+                cafe.setBase64Img(base64Img);
+            }
+        }
     }
 }
