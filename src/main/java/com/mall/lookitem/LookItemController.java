0bedb480af8d5ae7fbac99d866948c851bc0caab
YA.Xin <ttanyaya123@gmail.com>
2024-12-17 16:48:55 +0800

    ~

--- src/main/java/com/mall/lookitem/LookItemController.java
+++ src/main/java/com/mall/lookitem/LookItemController.java
@@ -21,59 +21,59 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/lookItem")
 @Controller
 public class LookItemController {
-	
-	
-	
-	@Autowired
-	private LookItemService lookItemService;
+ 
+ 
+ 
+ @Autowired
+ private LookItemService lookItemService;
 
-//	Base64
-	private void convertCoverImgToBase64(List<LookItemVO> items) {
-		for (LookItemVO item : items) {
-			if (item.getCoverImg() != null) {
-				String base64CoverImg = Base64.getEncoder().encodeToString(item.getCoverImg());
-				item.setBase64CoverImg(base64CoverImg); 
-			}
-		}
-	}
-	// 全部商品
-	@GetMapping("")
-	public String getAllLookItems(Model model) {
-		List<LookItemVO> items = lookItemService.getAll();
-		convertCoverImgToBase64(items);  
-		model.addAttribute("items", items);
-		return "front-end/lookitem/lookItem";
-	}
+// Base64
+ private void convertCoverImgToBase64(List<LookItemVO> items) {
+  for (LookItemVO item : items) {
+   if (item.getCoverImg() != null) {
+    String base64CoverImg = Base64.getEncoder().encodeToString(item.getCoverImg());
+    item.setBase64CoverImg(base64CoverImg); 
+   }
+  }
+ }
+ // 全部商品
+ @GetMapping("")
+ public String getAllLookItems(Model model) {
+  List<LookItemVO> items = lookItemService.getAll();
+  convertCoverImgToBase64(items);  
+  model.addAttribute("items", items);
+  return "front-end/lookitem/lookItem";
+ }
 
-	// 詳細資訊
-	@GetMapping("/oneItem/{itemId}")
-	public String getOneItem(@PathVariable("itemId") Integer itemId, Model model) {
-		LookItemVO item = lookItemService.getOneItem(itemId);
-		if (item != null && item.getCoverImg() != null) {
-			String base64CoverImg = Base64.getEncoder().encodeToString(item.getCoverImg());
-			item.setBase64CoverImg(base64CoverImg);
-		}
-		model.addAttribute("item", item);
-		return "front-end/lookitem/oneItem";
-	}
-	
+ // 詳細資訊
+ @GetMapping("/oneItem/{itemId}")
+ public String getOneItem(@PathVariable("itemId") Integer itemId, Model model) {
+  LookItemVO item = lookItemService.getOneItem(itemId);
+  if (item != null && item.getCoverImg() != null) {
+   String base64CoverImg = Base64.getEncoder().encodeToString(item.getCoverImg());
+   item.setBase64CoverImg(base64CoverImg);
+  }
+  model.addAttribute("item", item);
+  return "front-end/lookitem/oneItem";
+ }
+ 
 
-	// 查詢
-		@PostMapping("/listItem_ByCompositeQuery")
-	    public String listAllItem(HttpServletRequest req, Model model){
-	        Map<String, String[]> map = req.getParameterMap();  
-	        List<LookItemVO> list = lookItemService.getAll(map); 
-	        for (LookItemVO lookItem : list) {
-	            if (lookItem.getCoverImg() != null) {
-	                // Base64
-	                String base64CoverImg = Base64.getEncoder().encodeToString(lookItem.getCoverImg());
-	                lookItem.setBase64CoverImg(base64CoverImg);
-	        }            
-	        model.addAttribute("items", list); // 加到模型   
-	    } 
-		return "front-end/lookitem/lookItem"; 
-		}
-	        
-		
-		
+ // 查詢
+  @PostMapping("/listItem_ByCompositeQuery")
+     public String listAllItem(HttpServletRequest req, Model model){
+         Map<String, String[]> map = req.getParameterMap();  
+         List<LookItemVO> list = lookItemService.getAll(map); 
+         for (LookItemVO lookItem : list) {
+             if (lookItem.getCoverImg() != null) {
+                 // Base64
+                 String base64CoverImg = Base64.getEncoder().encodeToString(lookItem.getCoverImg());
+                 lookItem.setBase64CoverImg(base64CoverImg);
+         }            
+         model.addAttribute("items", list); // 加到模型   
+     } 
+  return "front-end/lookitem/lookItem"; 
+  }
+         
+  
+  
 }
